5f075dd4ac3612685258bb56458508fd40220d0d
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-09-12 23:13:17 -0400

    Added functionality to save new quotes to local data from a second quotes.html page

--- src/quote.js
+++ src/quote.js
@@ -1,14 +1,30 @@
-const quoteText = "The only limit to our realization of tomorrow will be our doubts of today.";
-const quoteSource = "Undelivered Address April 13, 1945";
-const quoteAuthor = "Franklin D. Roosevelt";
+const quoteText = document.getElementById("text");
+const quoteSource = document.getElementById("source");
+const quoteAuthor = document.getElementById("author");
 const output = document.getElementById("container-output");
 const quoteList = [];
 quoteList.push(
     {id: 1,
-        text: quoteText,
-        author: quoteAuthor,
-        source: quoteSource
+        text: "The only limit to our realization of tomorrow will be our doubts of today.",
+        author: "Franklin D. Roosevelt",
+        source: "Undelivered Address April 13, 1945"
     });
+
+const saveBtn = document.getElementById("save-btn");
+const exportBtn = document.getElementById("export-btn");
+saveBtn.addEventListener("click", ()=>{
+    quoteList.push({
+        id: quoteList.length+1,
+        text: quoteText.value,
+        author: quoteAuthor.value,
+        source: quoteSource.value
+    });
+    quoteText.value="";
+    quoteAuthor.value="";
+    quoteSource.value="";
+    displayQuote(quoteList[quoteList.length-1]);
+});
+exportBtn.addEventListener("click", saveQuotes);
 
 function displayQuote(quote){
     const {id, text, author, source} = quote;
@@ -34,5 +50,3 @@ displayQuote(quoteList[0]);
 function saveQuotes(){
     localStorage.setItem("quotes", JSON.stringify(quoteList));
 }
-
-saveQuotes();
