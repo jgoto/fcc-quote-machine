c57812cb36fa4cea6f651f9443a9ffa9208264cc
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-02-12 23:28:35 -0500

    Moved the quoteList to it's own file to improve the readability of the files

--- React-Quote-Machine/src/QutoeMachine.jsx
+++ React-Quote-Machine/src/QutoeMachine.jsx
@@ -1,35 +1,5 @@
 import React from "react"
-
-const quoteList = [{
-    text: "The man who does not read has no advantage over the man who cannot read.",
-    author: "Mark Twain",
-    qid: 1
-},
-{
-    text: "The only limit to our realization of tomorrow will be our doubts of today.",
-    author: "Franklin D. Roosevelt",
-    qid: 2
-},
-{
-    text: "Mistakes are always forgivable, if one has the courage to admit them.",
-    author: "Bruce Lee",
-    qid: 3
-},
-{
-    text: "Don’t forget to tell your favorite people that you love them.",
-    author: "Shirley Temple",
-    qid: 4
-},
-{
-    text: "Never let the fear of striking out keep you from playing the game",
-    author: "Babe Ruth",
-    qid: 5
-},
-{
-    text: "Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less.",
-    author: "Marie Curie",
-    qid: 6
-}]
+import quoteList from "./quoteList";
 
 const QuoteMachine = ({updateQuoteDetails}) => {
     const getRandomQuote = () =>{
