f48897ecf53d93b2ec6667516f0e42192ff53f34
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-02-12 21:36:27 -0500

    Created as a React project

---
+++ React-Quote-Machine/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+        <p>
+          Basic React App
+        </p>
+    </div>
+  )
+}
+
+export default App
